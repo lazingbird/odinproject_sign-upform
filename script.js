4f65720447cc920943d86ec6f7413cfe9cc10ad8
Lucas <lazingbird@protonmail.com>
2022-10-10 19:50:41 -0300

    finish project core

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function checkPass(form){
+    password = form.pw.value;
+    confirmPassword = form.confirmPw.value;
+
+    if(password != confirmPassword){
+        alert("Passwords do not match")
+    }
+
+}
